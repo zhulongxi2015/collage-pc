8bf867baf51a454ce0a681a1222949d6424fc920
ezhujam <james.zhu@ericsson.com>
2021-05-28 15:27:03 +0800

    add page logic.

--- resources/js/app.js
+++ resources/js/app.js
@@ -47,7 +47,5 @@ $(function(){
     if (MobileUA.SMART_PHONE) {
         // 移动端链接地址
         document.location.href ='http://m.gannanzz.com';
-    }else{
-        document.location.href='http://www.gannanzz.com';
     }
 });
